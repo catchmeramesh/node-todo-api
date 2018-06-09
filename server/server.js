296ab7d8385d4e75f560975d644688f9e8bd10b6
Ramesh <catchmeramesh1@gmail.com>
2018-06-09 15:26:25 -0500

    refatored, created todo and user api

--- server/server.js
+++ server/server.js
@@ -1,28 +1,41 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require('./db/mongoose');
+var {User} = require('./models/user');
+var {Todo} = require('./models/todo');
 
-var Todo = mongoose.model('Todo',{
-    text: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    },
-    completedAt: {
-        type: Number
-    }
+
+var app = express();
+
+app.use(bodyParser.json());
+
+
+app.post('/todos',(req,res)=>{
+    var todo =  new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc)=>{
+        res.send(doc);
+    },(err)=>{
+        res.status(404).send(err);
+    })
 });
 
-var newTodo = new Todo({
-    text: 'Third to do',
-    completed: false
-});
+app.post('/users',(req,res)=>{
+    var user = new User({
+        email: req.body.email
+    });
 
-newTodo.save().then((doc)=>{
-    console.log('Todo saved: ',doc);
-}, (err)=>{
-    console.log('Todo not saved: ',err);
-}
-);
+    user.save().then((doc)=>{
+        res.send(doc);
+    },(err)=>{
+        res.send(err);
+    });
+
+})
+
+app.listen('3000',()=>{
+    console.log('Server started and listening @ port 3000');
+});
