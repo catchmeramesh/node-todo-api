664d947e8ecc4f7685b5035c2a40c5e29b5762f1
Ramesh <catchmeramesh1@gmail.com>
2018-06-08 21:53:19 -0500

    Adding new file for fetching

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -8,26 +8,18 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,client)=>{
 
     var db = client.db('TodoApp');
 
-    // db.collection('Users').insertOne({
-    //     name: 'Mithran',
-    //     age: 6,
-    //     location: 'Ballwin'
-    // },(err,result)=>{
-    //     if(err){
-    //         return console.log('Insert Failed.');
-    //     }
-    //     console.log(JSON.stringify(result.ops,undefined,2));
-    // });
+    db.collection('Users').insertOne({
+        name: 'Mithran',
+        age: 6,
+        location: 'Ballwin'
+    },(err,result)=>{
+        if(err){
+            return console.log('Insert Failed.');
+        }
+        console.log(JSON.stringify(result.ops,undefined,2));
+    });
 
-    // db.collection('Users').find({name:'Mithran'}).toArray().then((docs)=>{
-    //     console.log(JSON.stringify(docs,undefined,2));
-    // },(err)=>{
-    //     console.log('Error fetching data.',err);
-    // })
-
-    db.collection('Todos').find().count().then((count)=>{
-        console.log('Total todos:',count);
-    })
+    
 
     client.close();
 });
