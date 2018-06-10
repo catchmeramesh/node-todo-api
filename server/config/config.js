1e048035995ec98ef17cc3abcfdc26ca9bbf49a2
Ramesh <catchmeramesh1@gmail.com>
2018-06-10 13:11:08 -0500

    Fixing connectivity issue to mongolabs db via Heroku

--- server/config/config.js
+++ server/config/config.js
@@ -3,13 +3,11 @@ var env = process.env.NODE_ENV || 'development';
 
 if (env === 'development'){
     process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp'
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
 } else if (env === 'test') {
     process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest'
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
 } else {
-    mongoose.connect('mongodb://Ramesh:Rsrjvs7U@ds153980.mlab.com:53980/todoapp', {
-        useMongoClient: true,
-    });
+    process.env.MONGODB_URI = 'mongodb://Ramesh:Rsrjvs7U@ds153980.mlab.com:53980/todoapp';
 }
 
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,18 @@ mongoose.Promise = global.Promise;
 
 // mongoose.connect('mongodb://ds153980.mlab.com:53980/todoapp');
 
-mongoose.connect(process.env.MONGODB_URI);
+if (process.env.PORT === 3000) {
+    mongoose.connect(process.env.MONGODB_URI)
+}else{
+    mongoose.connect(process.env.MONGODB_URI,{
+        useMongoClient: true
+    });
+}
+
+
+
+// mongoose.connect('mongodb://Ramesh:Rsrjvs7U@ds153980.mlab.com:53980/todoapp', {
+//         useMongoClient: true,
+//     });
 
 module.exports = {mongoose}
