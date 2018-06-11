3547ba059fe166a7a4a11eb5ba674932f08343be
Ramesh <catchmeramesh1@gmail.com>
2018-06-11 18:21:04 -0500

    Added authenticate middleware

--- server/models/user.js
+++ server/models/user.js
@@ -32,6 +32,25 @@ var UserSchema = new mongoose.Schema({
     }]
 });
 
+UserSchema.statics.findByToken = function(token){
+    var User = this;
+    var decoded;
+
+    try{
+
+        decoded = jwt.verify(token, 'seceret123');
+
+    }catch(e){
+       return Promise.reject();
+    }
+
+    return User.findOne({
+        _id: decoded._id,
+        'tokens.token':token,
+        'tokens.access': 'auth'
+    });
+
+};
 
 UserSchema.methods.generateAuthToken = function() {
     var user = this;
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const _ = require('lodash');
 var {mongoose} = require('./db/mongoose');
 var {User} = require('./models/user');
 var {Todo} = require('./models/todo');
+var {authenticate} = require('./middleware/authenticate')
 
 
 var app = express();
@@ -131,6 +132,10 @@ app.post('/users',(req,res)=>{
         res.status(400).send(err);
     });
 });
+
+app.get('/users/me',authenticate,(req,res)=>{
+   res.send(req.user);
+})
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server started and listening @ port ${process.env.PORT}`);
